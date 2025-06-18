9b51aef73b3239af9849a8203b5660ac6b2e3ba7
Mohammed Malik <malikmohammed@2067>
2025-06-18 22:02:57 +0530

    Graph represented as an adjacency list (dictionary)

---
+++ dfs.py
@@ -0,0 +1,8 @@
+# Graph represented as an adjacency list (dictionary)
+graph = { 
+    'A': ['B', 'C'], 
+    'B': ['A', 'C', 'D'], 
+    'C': ['A', 'B', 'E'], 
+    'D': ['B', 'E'], 
+    'E': ['C', 'D']
+}
